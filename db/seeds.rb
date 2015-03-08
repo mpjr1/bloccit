29b202d33b77b5745b343d49a09f612494a374c1
Mario <mapg.jr@gmail.com>
2015-03-08 21:18:19 +0000

    Completed Seed-Data Assigment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ require 'faker'
+ 
+post = Post.create!(
+  title: "A New Unique Post",
+  body: "blah blah blah"
+)
+
+Comment.create!(
+  post: post,
+  body: "Has anyone really been far even as decided to use even go want to do look more like?"
+ )
+ 
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
