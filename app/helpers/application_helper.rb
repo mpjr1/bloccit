a2f536bd519f0f153283362f51017c22b9458d44
Mario <mapg.jr@gmail.com>
2015-03-11 15:09:42 +0000

    Complete crud assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  
+  def my_name
+    "Mario Peña Jr"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,16 @@
  require 'faker'
  
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
- 
+Post.find_or_create_by!(
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+)
+
+Advertisement.find_or_create_by!(
+    title:  Faker::Lorem.sentence,
+    copy:   Faker::Lorem.paragraph,
+    price:  Faker::Commerce.price
+)
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+ puts "#{Advertisement.count} advertisements created"
